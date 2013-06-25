b6828c23d38fe7aac1ae25d33c51c52eb129c300
liuyanghejerry <liuyanghejerry@126.com>
2013-06-25 09:37:53 +0800

    update: some basic info.
    Signed-off-by: liuyanghejerry <liuyanghejerry@126.com>

--- nullplace.js
+++ nullplace.js
@@ -12,6 +12,7 @@ function createWritableNullDevice() {
 function createReadableNullDevice() {
   // TODO: add more platforms
   if (process.platform === 'win32') {
+    // FIXME: I don't believe Windows can handle a readable nul
     return fs.createReadStream('nul');
   }else{
     return fs.createReadStream('/dev/null');
